e36e48094fd2448c85c6e026763d0bf9706b69e5
김지현 <kjhstar2486@naver.com>
2025-12-01 18:57:54 +0900

    ⚡️ Add: ASP NET CORE 파일 생성 (VS)

--- ProjectG_WebApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjectG_WebApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
